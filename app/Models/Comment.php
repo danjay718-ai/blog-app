8c5fa5aa5febf013121f83b87a6e168bff58942a
danjay718-ai <dannverjay.lagramada718@gmail.com>
2025-08-10 23:59:19 +0800

    Initial commit

---
+++ app/Models/Comment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'content',
+        'post_id',
+        'user_id',
+        'status',
+    ];
+
+    public function post() {   
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeActive($query) {
+        return $query->where('status', 'active');
+    }
+}
